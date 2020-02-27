045bfba57372735c638c47c3568480e60c14b9c6
Pamela Tao <pamelatao12@gmail.com>
2020-02-26 20:15:43 -0500

    push event calendar updates

--- src/calendar.js
+++ src/calendar.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./calendar.css";
+import EventPanel from "./eventPanel";
 
 const Calendar = () => {
-  return null;
+  const [state, setState] = useState({
+    eventsList: [
+      {
+        title: "Dinner with David",
+        date: "08-28-2020",
+        time: "07:00",
+        location: "Eleven Madison Park"
+      },
+      {
+        title: "Dinner with Pamela",
+        date: "09-02-2020",
+        time: "6:30",
+        location: "TBD"
+      }
+    ]
+  });
+
+  return (
+    <div className="calendar">
+      <h1>My events</h1>
+      <div className="events">
+        <ul className="eventListUl">
+          {state.eventsList.map((eventDetail, i) => (
+            <li className="eventList" key={i}>
+              <EventPanel eventDetails={eventDetail} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Calendar;
---
+++ src/eventPanel.js
@@ -0,0 +1,57 @@
+import React from "react";
+import Collapsible from "react-collapsible";
+import "./eventPanel.css";
+
+const EventPanel = ({ eventDetails }) => {
+  const dateMap = {
+    "01": "Jan",
+    "02": "Feb",
+    "03": "Mar",
+    "04": "Apr",
+    "05": "May",
+    "06": "June",
+    "07": "July",
+    "08": "Aug",
+    "09": "Sept",
+    "10": "Oct",
+    "11": "Nov",
+    "12": "Dec"
+  };
+
+  const date = eventDetails.date;
+
+  const month = date.substring(0, 2);
+  const day = date.substring(3, 5);
+  const year = date.substring(6);
+
+  {
+    console.log(month);
+    console.log(dateMap[month]);
+  }
+
+  const triggerElement = (
+    <>
+      <div className="date">
+        <span className="binds"></span>
+        <span className="month">{dateMap[month]}</span>
+        {/* fill in event details with object values */}
+        <h1 className="day">28</h1>
+      </div>
+      <p className="eventTitle">{eventDetails.title}</p>
+    </>
+  );
+  return (
+    <Collapsible trigger={triggerElement} triggerTagName="div">
+      <p>
+        This is the collapsible content. It can be any element or React
+        component you like.
+      </p>
+      <p>
+        It can even be another Collapsible component. Check out the next
+        section!
+      </p>
+    </Collapsible>
+  );
+};
+
+export default EventPanel;
