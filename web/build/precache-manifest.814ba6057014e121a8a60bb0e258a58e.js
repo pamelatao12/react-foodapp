932f43b5863200f1461de54ccbbd59afe32b23f6
Pamela Tao <pamelatao12@gmail.com>
2020-05-08 13:44:43 -0400

    removed raw tag

---
+++ web/build/precache-manifest.814ba6057014e121a8a60bb0e258a58e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60237e395bd576a0c2c839b82c822b33",
+    "url": "/react-foodapp/index.html"
+  },
+  {
+    "revision": "5bcfcfe965abb671f3b2",
+    "url": "/react-foodapp/static/css/main.279c7778.chunk.css"
+  },
+  {
+    "revision": "72e7ee3a411c55fd9d4b",
+    "url": "/react-foodapp/static/js/2.278aa666.chunk.js"
+  },
+  {
+    "revision": "46f63f8626516ab37e48a023f0c6fcd4",
+    "url": "/react-foodapp/static/js/2.278aa666.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5bcfcfe965abb671f3b2",
+    "url": "/react-foodapp/static/js/main.c379bffd.chunk.js"
+  },
+  {
+    "revision": "d923bdb436e97233e679",
+    "url": "/react-foodapp/static/js/runtime-main.9bf7299a.js"
+  }
+]);
