3ef7183a00f554d3a4db6caa7b3808bf41fe9789
Pamela Tao <pamelatao12@gmail.com>
2020-05-29 18:38:47 -0400

    added input validations on create account form

--- web/src/signedOut/createAccount.jsx
+++ web/src/signedOut/createAccount.jsx
@@ -16,18 +16,48 @@ const CreateAccount = () => {
     });
   };
 
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
 
   const handleSubmit = async e => {
     e.preventDefault();
 
-    const { email, password } = input;
+    const { first, last, email, password } = input;
+    let inputsHaveError = false;
 
-    if (password && password.length < 6) {
+    if (!password || password.length < 6) {
       setPasswordError(true);
+      inputsHaveError = true;
+    } else {
+      setPasswordError(false);
+    }
+
+    if (!first) {
+      setFirstNameError(true);
+      inputsHaveError = true;
+    } else {
+      setFirstNameError(false);
+    }
+
+    if (!last) {
+      setLastNameError(true);
+      inputsHaveError = true;
+    } else {
+      setLastNameError(false);
+    }
+
+    if (!email) {
+      setEmailError(true);
+      inputsHaveError = true;
+    } else {
+      setEmailError(false);
+    }
+
+    if (inputsHaveError) {
       return;
     }
-    setPasswordError(false);
 
     await createAccount(email, password);
     setAccountCreated(true);
@@ -39,6 +69,107 @@ const CreateAccount = () => {
   const handleViewPassword = () => {
     pwInput === "password" ? setPwInput("text") : setPwInput("password");
     viewSetting === "🙈" ? setViewSetting("🙉") : setViewSetting("🙈");
+  };
+
+  const handleFirstNameInput = () => {
+    if (!firstNameError) {
+      return (
+        <div className={styles.input}>
+          <label className={styles.label}>First Name*</label>
+          <input
+            className={styles.textbox}
+            type="text"
+            name="first"
+            value={input.first ? input.first : ""}
+            onChange={handleInputChange}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <div className={styles.input}>
+            <label className={styles.label}>First Name*</label>
+            <input
+              className={styles.textboxError}
+              type="text"
+              name="first"
+              value={input.first ? input.first : ""}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className={styles.errorMessage}>
+            *First name is a required field
+          </div>
+        </>
+      );
+    }
+  };
+
+  const handleLastNameInput = () => {
+    if (!lastNameError) {
+      return (
+        <div className={styles.input}>
+          <label className={styles.label}>Last Name*</label>
+          <input
+            className={styles.textbox}
+            type="text"
+            name="last"
+            value={input.last ? input.last : ""}
+            onChange={handleInputChange}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <div className={styles.input}>
+            <label className={styles.label}>Last Name*</label>
+            <input
+              className={styles.textboxError}
+              type="text"
+              name="last"
+              value={input.last ? input.last : ""}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className={styles.errorMessage}>
+            *Last name is a required field
+          </div>
+        </>
+      );
+    }
+  };
+
+  const handleEmailInput = () => {
+    if (!emailError) {
+      return (
+        <div className={styles.input}>
+          <label className={styles.label}>Email*</label>
+          <input
+            className={styles.textbox}
+            type="email"
+            name="email"
+            onChange={handleInputChange}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <>
+          <div className={styles.input}>
+            <label className={styles.label}>Email*</label>
+            <input
+              className={styles.textboxError}
+              type="email"
+              name="email"
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className={styles.errorMessage}>*Email is a required field</div>
+        </>
+      );
+    }
   };
 
   const handlePasswordLength = () => {
@@ -91,33 +222,9 @@ const CreateAccount = () => {
       </Link>
       <div className={styles.createAccountHeader}>Create Account</div>
       <div className={styles.inputWrapper}>
-        <div className={styles.input}>
-          <label className={styles.label}>First Name*</label>
-          <input
-            className={styles.textbox}
-            type="text"
-            name="first"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className={styles.input}>
-          <label className={styles.label}>Last Name*</label>
-          <input
-            className={styles.textbox}
-            type="text"
-            name="last"
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className={styles.input}>
-          <label className={styles.label}>Email*</label>
-          <input
-            className={styles.textbox}
-            type="text"
-            name="email"
-            onChange={handleInputChange}
-          />
-        </div>
+        {handleFirstNameInput()}
+        {handleLastNameInput()}
+        {handleEmailInput()}
         {handlePasswordLength()}
       </div>
       <button className={styles.button} type="submit">
