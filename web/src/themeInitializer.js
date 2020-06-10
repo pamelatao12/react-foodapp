ac1cff75bebb7f4c75febef4c5c1e7e6fd45a0d9
Pamela Tao <pamelatao12@gmail.com>
2020-06-10 18:56:32 -0400

    fixed dismiss when click outside of popover

--- web/src/themeInitializer.js
+++ web/src/themeInitializer.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Client as Styletron } from "styletron-engine-atomic";
 import { Provider as StyletronProvider } from "styletron-react";
+import { BaseProvider, LightTheme } from "baseui";
 
 // Set up styling engine for baseUI
 const engine = new Styletron();
 
 export const ThemeInitializer = ({ children }) => {
-  return <StyletronProvider value={engine}>{children}</StyletronProvider>;
+  return (
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>{children}</BaseProvider>
+    </StyletronProvider>
+  );
 };
