310fe483517d99fcea891c19b77d269d04439bce
Pamela Tao <pamelatao12@gmail.com>
2020-06-16 19:05:33 -0400

    nulled date and time picker on event form

--- web/src/addEventForm.js
+++ web/src/addEventForm.js
@@ -1,40 +1,53 @@
 import React, { useState } from "react";
-import styles from "./addEventForm.css";
+// import styles from "./addEventForm.css";
 import { useStyletron } from "baseui";
 import { FormControl } from "baseui/form-control";
 import { DatePicker } from "baseui/datepicker";
 import { TimePicker } from "baseui/timepicker";
 import { TimezonePicker } from "baseui/timezonepicker";
+import { Input } from "baseui/input";
 
 const AddEventForm = () => {
   const [css, theme] = useStyletron();
-  const [date, setDate] = useState(undefined);
+  const [date, setDate] = useState(null);
   const [zone, setZone] = useState(null);
+  const [title, setTitle] = useState("");
+  console.log("date is", date);
 
   return (
-    <div className={css({ display: "flex" })}>
-      <div className={styles.form}>
-        <input
-          name="term"
-          id={styles.input}
-          type="text"
-          placeholder="Sushi... burgers... pasta..."
-          value="input"
-        />
-        <div
-          className={css({
-            display: "flex",
-            flexDirection: "row"
-          })}
-        >
+    <div className={css({})}>
+      <div
+        className={css({
+          fontWeight: "bold",
+          marginBottom: "5px"
+        })}
+      >
+        Event Title
+      </div>
+      <Input
+        value={title}
+        onChange={e => setTitle(e.target.value)}
+        placeholder="Event Title"
+      />
+      <div
+        className={css({
+          display: "flex",
+          flexDirection: "row"
+        })}
+      >
+        <div className={css({ display: "flex" })}>
           <div
             className={css({
-              width: "120px",
+              width: "160px",
               marginRight: theme.sizing.scale300
             })}
           >
             <FormControl label="Date">
-              <DatePicker value={date} onChange={({ date }) => setDate(date)} />
+              <DatePicker
+                value={date}
+                placeholder="YYYY/MM/DD"
+                onChange={({ date }) => setDate(date)}
+              />
             </FormControl>
           </div>
 
@@ -45,21 +58,7 @@ const AddEventForm = () => {
             })}
           >
             <FormControl label="Time">
-              <TimePicker value={date} onChange={setDate} />
-            </FormControl>
-          </div>
-
-          <div
-            className={css({
-              width: "340px"
-            })}
-          >
-            <FormControl label="Timezone">
-              <TimezonePicker
-                date={date}
-                value={zone && zone.id}
-                onChange={setZone}
-              />
+              <TimePicker nullable value={date} onChange={setDate} />
             </FormControl>
           </div>
         </div>
