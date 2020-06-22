589987922df000635513c0c85754c8568aa024da
Pamela Tao <pamelatao12@gmail.com>
2020-06-22 19:50:23 -0400

    get unique identifier key of event node in database

--- server/src/modules/events/index.js
+++ server/src/modules/events/index.js
@@ -1,33 +1,43 @@
 const database = require("../firebase/index");
 const firebaseAdmin = require("firebase-admin");
 
-const getUserRecord = async email => {
-  return firebaseAdmin
-    .auth()
-    .getUserByEmail(email)
-    .then(function(userRecord) {
-      // See the UserRecord reference doc for the contents of userRecord.
-      console.log("Successfully fetched user data:", userRecord.toJSON());
-      return userRecord.toJSON();
-    })
-    .catch(function(error) {
-      console.log("Error fetching user data:", error);
-    });
+const getEventRecord = async eventId => {
+  return (await database.read(`events/${eventId}`)).val();
 };
 
+// create event in database, link event to user who created it
+// return list of user's events
 const createEvent = async (req, res) => {
   const { title, date, location, guests, notes } = req.query;
   console.log("attempting to add event to database: ", title);
   // const event = await getEventRecord(email);
-  database.push("events/", {
+  const key = await database.push("events/", {
     title: title,
     date: date,
     location: location,
-    guests: guestList,
+    guests: guests,
     notes: notes
   });
+  const eventRecord = await getEventRecord(key);
+  console.log("event:", eventRecord);
+  addEventToUserRecord(key);
   console.log("event added to database");
   // return event;
+};
+
+const addEventToUserRecord = async eventId => {
+  const user = firebaseAdmin.auth().onAuthStateChanged(async user => {
+    if (user) {
+      // User is signed in.
+      console.log("curr user: ", user);
+      const uid = user.uid;
+      const eventsList = (await database.read(`users/${uid}/events`)).val();
+      console.log(eventsList);
+    } else {
+      // No user is signed in.
+      console.log("User not signed in");
+    }
+  });
 };
 
 const initializeEventRoutes = router => {
